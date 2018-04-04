be9d5b16f55794a66ae621fd49e7c64910041a34
Justas Dabrila <justasdabrila@gmail.com>
2018-04-04 12:21:30 +0000

    scanmem: added license headers, added back Global+ struct

--- scanmem.py
+++ scanmem.py
@@ -1,3 +1,21 @@
+'''
+    Python3 bindings for scanmem
+    Copyright (C) 2018 Justas Dabrila <justasdabrila@gmail.com>
+
+    This library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 from ctypes import *
 import ctypes.util
 import atexit
@@ -10,9 +28,35 @@ if libfile is None:
 backend = CDLL(libfile)
 
 backend.sm_init()
-backend.sm_execcommand.argtypes = [POINTER(Globals), c_char_p]
+backend.sm_execcommand.argtypes = [c_void_p, c_char_p]
 backend.sm_execcommand.restype = c_bool
 backend.sm_init.restype = c_bool
+
+class GlobalOptions(Structure):
+    _fields_ = [
+            ("alignment", c_ushort),
+            ("debug", c_ushort),
+            ("backend", c_ushort),
+            ("scan_data_type", c_uint), # scan_data_type_t
+            ("region_scan_level", c_uint), #region_scan_level_t
+            ("dump_with_ascii", c_ushort),
+            ("reverse_endianness", c_ushort)
+    ]
+
+class Globals(Structure):
+    _fields_ = [
+            ("exit", c_uint, 1),
+            ("target", c_int), # pid_t
+            ("matches", c_void_p), # matches_and_old_values_array*
+            ("num_matches", c_ulong),
+            ("scan_progress", c_double),
+            ("stop_flag", c_bool),
+            ("regions", c_void_p), # list_t*
+            ("commands", c_void_p), #  list_t*
+            ("current_cmdline", c_char_p),
+            ("printversion", c_void_p), # void (*printversion)(FILE *outfd);
+            ("options", GlobalOptions)
+    ]
 
 class Scanmem():
 
